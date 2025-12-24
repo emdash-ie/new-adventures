31f51b3ce6a02fdac4732605b78d751b1739e101
Emily Bourke <emily@emdash.ie>
2025-12-24 14:31:49 +0000

    Serve org files from given directory

--- src/server/Main.hs
+++ src/server/Main.hs
@@ -1,29 +1,49 @@
+{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 module Main where
 
-import Servant
+import Prelude hiding (readFile)
 import Data.Aeson (ToJSON(..), (.=), object)
+import Data.ByteString (readFile)
+import Data.List (isSuffixOf)
 import Data.List.NonEmpty (NonEmpty(..))
 import Data.Map (Map)
 import Data.Map qualified as Map
+import Data.Maybe (catMaybes)
 import Data.Org
-import Data.Text
+import Data.Traversable (for)
+import Data.Text (Text)
+import Data.Text qualified as Text
+import Data.Text.Encoding (decodeUtf8)
 import GHC.Generics
 import Network.Wai.Handler.Warp (run)
+import Servant
+import System.Directory (listDirectory)
+import System.Environment (getArgs)
+
+main :: IO ()
+main = do
+  dir : _ <- getArgs
+  filePaths <- listDirectory dir
+  let orgFilePaths = filter (isSuffixOf ".org") filePaths
+  orgFiles <- fmap (Map.fromList . catMaybes) $ for orgFilePaths \f -> do
+    bs <- readFile f
+    return (fmap (Text.pack f,) (org (decodeUtf8 bs)))
+  run 8014 (app orgFiles)
 
 type ToDoAPI = "todos" :> Get '[JSON] (Map Text OrgFile)
 
-server :: Server ToDoAPI
-server = return exampleTodos
+server :: Map Text OrgFile -> Server ToDoAPI
+server = return
 
 todoAPI :: Proxy ToDoAPI
 todoAPI = Proxy
 
-app :: Application
-app = serve todoAPI server
+app :: Map Text OrgFile -> Application
+app = serve todoAPI . server
 
 exampleTodos :: Map Text OrgFile
 exampleTodos = Map.singleton "test.org" (OrgFile Map.empty (OrgDoc [Paragraph (Plain "hello" :| [])] []))
@@ -57,6 +77,3 @@ instance ToJSON RepeatMode
 deriving instance Generic DelayMode
 instance ToJSON DelayMode
 
-
-main :: IO ()
-main = run 8014 app
